a202ba153c32f206fd79dc7fa987ce9be481068a
Hunter Pimparatana <98248872+HoonterP@users.noreply.github.com>
2022-10-29 23:38:00 -0600

    Adding Project Files

---
+++ retrieve_employee.js
@@ -0,0 +1,18 @@
+let { MongoClient } = require("mongodb")
+uri = "mongodb://127.0.0.1:27017"
+// uri = "mongodb://localhost:27017"
+client = new MongoClient(uri)
+async function run(){
+    // This looks like a try and except, similar to python.
+    try{
+        await client.connect()
+        database = client.db('PimparatanaHunter')
+        table = database.collection('employees')
+        query = {}
+        row = await table.find(query) 
+        await row.forEach(function(row) { console.log(row) })
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
